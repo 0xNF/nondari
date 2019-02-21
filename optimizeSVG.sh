af1ddb1726346ce56dbab20d46d66fa7c313b842
0xNF <nflower+github@articulateinsights.com>
2019-02-21 07:50:18 -0800

    svg optimizing step on build

---
+++ optimizeSVG.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# make necessary dirs first
+mkdir src/img/ingredients/
+mkdir src/img/glasses/
+
+# copy the raw SVGs
+cp RawSVGs/glasses/* src/img/glasses/
+cp RawSVGs/ingredients/droplet/* src/img/ingredients/
+cp RawSVGs/ingredients/drops/* src/img/ingredients/
+cp RawSVGs/ingredients/ice/* src/img/ingredients/
+cp RawSVGs/ingredients/slices/* src/img/ingredients/
+cp RawSVGs/ingredients/sugar/* src/img/ingredients/
+cp RawSVGs/ingredients/twists/* src/img/ingredients/
+cp RawSVGs/ingredients/wedges/* src/img/ingredients/
+cp RawSVGs/ingredients/*.svg src/img/ingredients/
+
+# optimize the svgs
+node_modules/svgo/bin/svgo -f ./src/img/glasses/ --multipass
+node_modules/svgo/bin/svgo -f ./src/img/ingredients/ --multipass
